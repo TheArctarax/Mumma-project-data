6ee5253e24c9a3532c17d172bad15a11d10e1a41
alvin.li <alvin.li@ligo.org>
2020-06-16 08:47:16 -0700

    Added testing script for pycbc

---
+++ testing/pycbc_test.py
@@ -0,0 +1,16 @@
+from pycbc.waveform import get_td_waveform
+import pylab
+
+hp, hc = get_td_waveform(approximant="SEOBNRv4_opt",
+                         mass1=10,
+                         mass2=10,
+                         delta_t=1.0/4096,
+                         f_lower=30)
+
+pylab.plot(hp.sample_times, hp, label='Plus Polarization')
+pylab.plot(hp.sample_times, hc, label='Cross Polarization')
+pylab.xlabel('Time (s)')
+pylab.legend()
+pylab.grid()
+pylab.show()
+

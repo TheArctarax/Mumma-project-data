22f030fe8720a80cd1e84260b34df7af15aa8c89
alvin.li <alvin.li@ligo.org>
2020-06-25 10:36:02 -0700

    updated memory code

--- memory_effect.py
+++ memory_effect.py
@@ -2,23 +2,24 @@ from __future__ import division, print_function
 import gwmemory
 from matplotlib.pyplot import figure
 import numpy as np
+from matplotlib.pyplot import *
+np.seterr(divide='ignore',invalid='ignore')
+
 
 fig = figure(figsize=(12, 4))
 
-q = 1
+q = 1.
 S1 = [0, 0, 0]
 S2 = [0, 0, 0]
 
 colours = ['r', 'b', 'g', 'k']
 
-for ii, model in enumerate(['NRSur7dq2', 'IMRPhenomD', 'SEOBNRv4', 'MWM']):
-    h_mem, times = gwmemory.time_domain_memory(q=q, S1=S1, S2=S2, MTot=60., distance=400.,
-                                             model=model, inc=np.pi/2, pol=0)
-
-    plot(times, h_mem['plus'] - h_mem['plus'][np.argmin(abs(times+0.25))],
-         linestyle='-', color=colours[ii], label=model)
-    plot(times, h_mem['cross'] - h_mem['cross'][np.argmin(abs(times+0.25))],
-         linestyle='--', color=colours[ii])
+for ii, model in enumerate(['IMRPhenomD']):
+	delta_t = 1.0 / 4096
+	times = np.linspace(0, delta_t , 2)
+	h_mem, times = gwmemory.time_domain_memory(model=model, q=q, spin_1=S1, spin_2=S2, total_mass=60.,distance=400., inc=np.pi/2, phase=0, times=times)
+	plot(times, h_mem['plus'] - h_mem['plus'][np.argmin(abs(times+0.25))],linestyle='-', color=colours[ii], label=model)
+	plot(times, h_mem['cross'] - h_mem['cross'][np.argmin(abs(times+0.25))],linestyle='--', color=colours[ii])
         
 xlabel('$t (s)$')
 ylabel('$\delta h$')
@@ -26,6 +27,4 @@ legend(loc='upper left', fontsize=20)
 
 xlim(-0.25, 0.02)
 
-tight_layout()
-show()
-close()
+savefig("trial.pdf")

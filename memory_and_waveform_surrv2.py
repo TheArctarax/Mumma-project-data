e5b05ab22b26b358f9c231ffd8e87f5801e1fa0b
alvin.li <alvin.li@ligo.org>
2020-06-30 04:34:46 -0700

    Modified memory_and_waveform_surrv2.py

--- memory_and_waveform_surrv2.py
+++ memory_and_waveform_surrv2.py
@@ -24,15 +24,25 @@ end_time=0.02
 times = np.linspace(start_time, end_time, 10001)
 
 
-# GW memory-less definition
-hp = gwmemory.waveforms.surrogate.Surrogate(q=q, spin_1=S1, spin_2=S2, total_mass=M, distance=400, times=times)
+# GW waveform with memory definition
+# The sub-function waveforms.surrogate.Surrogate generates a surrogate object.
+surr = gwmemory.waveforms.surrogate.Surrogate(q=q, spin_1=S1, spin_2=S2, total_mass=M, distance=400, times=times)
 
+
+# GW waveform only definition
+# A surrogate object has the following attributes: time_domain_memory (a 
+# pycbc.timeseries object that has both the ['plus'] and ['cross'] sub-arrays), 
+# and time_domain_memory which also has ['plus'] and ['cross']). Calling
+# these attributes returns both the pycbc timesseries and the sampling time
+# arrays (which you store it as times here).
+oscillatory, times = surr.time_domain_oscillatory(inc=inc, phase=pol)
 
 # GW memory definition
-memory, times = hp.time_domain_oscillatory(inc=inc, phase=pol)
-
+memory, times = surr.time_domain_memory(inc=inc, phase=pol)
 
 # Plot of GW memory
+# By using the above attributes, you can easily plot out the memory,
+# original waveform and waveform + memory. 
 fig = figure(figsize=(12, 6))
 plot(times, memory['plus']*(10**22), color='r')
 axhline(0, linestyle=':', color='k')
@@ -52,7 +62,7 @@ close()
 
 # Plot of memory-less waveform
 fig = figure(figsize=(12, 6))
-plot(times, hp*(10.0**22.0), color='r')
+plot(times, oscillatory['plus']*(10.0**22.0), color='r')
 axhline(0, linestyle=':', color='k')
 xlim(-0.5, 0.05)
 xlabel('Time (s)')
@@ -72,8 +82,9 @@ close()
 fig = figure(figsize=(12, 6))
 
 fig.add_subplot(2, 1, 2)
-plot(times, (hp[:] + memory['plus'][:])*(10.0**22.0), color='r', label=r'Waveform $\plus$ Memory')
-plot(times, hp*(10.0**22.0), linestyle='--' , color='tab:purple', label='Original Waveform')
+plot(times, (oscillatory['plus']+memory['plus'])*(10.0**22.0), color='r', label=r'Waveform $\plus$ Memory')
+plot(times, oscillatory['plus']*(10.0**22.0), linestyle='--' , color='tab:purple', label='Original Waveform')
+plot(times, memory['plus']*(10.0**22.0), linestyle='dotted', color='b', label='Memory')
 axhline(0, linestyle=':', color='k')
 xlim(-0.04, 0.015)
 xlabel('Time (s)')
@@ -82,13 +93,16 @@ legend(loc='upper left')
 rc('xtick', labelsize=12)
 rc('ytick', labelsize=12)
 rc('axes', labelsize=14)
+#tight_layout()
+#show()
+#close()
 
 
 fig.add_subplot(2, 1, 1)
-plot(times, (hp[:] + memory['plus'][:])*(10.0**22.0), color='r', label=r'Waveform $\plus$ Memory')
-plot(times, memory['plus']*(10.0**22.0), linestyle='--', color='b', label='Memory')
+plot(times, (oscillatory['plus'][:] + memory['plus'][:])*(10.0**22.0), color='r', label=r'Waveform $\plus$ Memory')
+plot(times, memory['plus']*(10.0**22.0), linestyle='dotted', color='b', label='Memory')
 axhline(0, linestyle=':', color='k')
-xlim(-0.3, 0.02)
+xlim(right=times[-1])
 ylabel(r'$h_\plus$ $[10^{-22}]$')
 legend(loc='upper left')
 rc('xtick', labelsize=12)

fd8e75ddefa7e3cd68876a68e3e20953257c3f64
TheArctarax <darinmumma303@gmail.com>
2020-07-01 13:30:21 -0400

    O2 noise curve for L1, H1, and V1

--- noise_curve.py
+++ noise_curve.py
@@ -1,43 +1,51 @@
-# Here I am importing pandas, an often-used data handling python
-# package.
-#import ssl
-#context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-import matplotlib
-matplotlib.use('Agg') 
+#HereIamimportingpandas,anoften-useddatahandlingpython
+#package.
+#importssl
+#context=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import requests
 import pandas as pd
 import wget
-#rcParams["font.family"] = "Times New Roman"
+import itertools
+from matplotlib.pyplot import rcParams
+rcParams["font.family"]="TimesNewRoman"
+rcParams['axes.unicode_minus'] = False
 
 
-# download L1 strain data from O2 off LIGO DCC. This is only required to be run once. Otherwise, duplicate files will be downloaded.
-
-#url = 'https://dcc.ligo.org/public/0156/G1801952/001/2017-08-06_DCH_C02_L1_O2_Sensitivity_strain_asd.txt'
-#wget.download(url, './L1_O2_Sensitivity_strain_asd.txt')
-
-#r = requests.get(url, allow_redirects=True)
-#open('l1o2strain.txt', 'wb').write(r.content)
-
-# read the resulting .txt file using pandas. I here named the columns as frequencies and asd.
-df = pd.read_csv('L1_O2_Sensitivity_strain_asd.txt', sep="\t", index_col=False, header=None, names=["frequency", "asd"])
-print(df)
+# download L1 strain data from O2 off LIGO DCC.This is only required to be run once. Otherwise, duplicate files will be downloaded.
+#
+#urls=('https://dcc.ligo.org/public/0156/G1801952/001/2017-08-06_DCH_C02_L1_O2_Sensitivity_strain_asd.txt',
+#     'https://dcc.ligo.org/public/0156/G1801950/001/2017-06-10_DCH_C02_H1_O2_Sensitivity_strain_asd.txt',
+#     'https://dcc.ligo.org/public/0157/P1800374/001/Hrec_hoft_V1O2Repro2A_16384Hz.txt')
+#names=('./L1_O2_Sensitivity_strain_asd.txt', './H1_O2_Sensitivity_strain_asd.txt', './V1_O2_Sensitivity_strain_asd.txt')
+#
+#for files, fnames in itertools.izip(urls, names):
+#    wget.download(files, fnames)
 
 
-#f = open('l1o2strain.txt','r')
-#ldata = f.read()
-#f.close()
+# read the resulting .txt file using pandas. I renamed the columns as frequencies and asd.
+dfl=pd.read_csv('L1_O2_Sensitivity_strain_asd.txt',sep="\t",index_col=False,header=None,names=["frequency","asd"])
+dfh=pd.read_csv('H1_O2_Sensitivity_strain_asd.txt',sep="\t",index_col=False,header=None,names=["frequency","asd"])
+dfv=pd.read_csv('V1_O2_Sensitivity_strain_asd.txt',sep="\t",index_col=False,header=None,names=["frequency","asd"])
+
 
 # plot the data
-#fig = figure(figsize=(6, 6))
-plt.plot(df['frequency'], df['asd'], color='b')
-plt.xlim(20, 1400)
-plt.ylim(5e-24, 1e-20)
-plt.xlabel('frequency (Hz)')
-plt.ylabel(r'noise strain [$Hz^{-1/2}$]')
+plt.plot(dfl['frequency'], dfl['asd'], color='b')
+plt.plot(dfh['frequency'], dfh['asd'], color='r')
+plt.plot(dfv['frequency'], dfv['asd'], color='tab:purple')
+plt.xlim(20,1400)
+#plt.ylim(5e-24,1e-20)
+plt.xlabel('Frequency [$Hz$]')
+plt.ylabel(r'Noise Strain [$Hz^{-1/2}$]')
 plt.xscale('log')
 plt.yscale('log')
-#plt.rc('xtick', labelsize=12)
-#plt.rc('ytick', labelsize=12)
-#plt.rc('axes', labelsize=14)
+plt.rc('xtick',labelsize=12)
+plt.rc('ytick',labelsize=12)
+plt.rc('axes',labelsize=14)
+
 plt.savefig("noise_curve.pdf")
+
+plt.tight_layout()
+plt.show()
+plt.close()
+

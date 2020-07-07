e754438884d66025a9c83af92f3c20806c7d5342
TheArctarax <darinmumma303@gmail.com>
2020-07-07 15:59:57 -0400

    reformatted the bilby code

--- bilby_memory_model.py
+++ bilby_memory_model.py
@@ -1,86 +1,106 @@
 from __future__ import division, print_function
-from pycbc.waveform import get_fd_waveform
 import numpy as np
 import bilby
 import gwmemory
-np.seterr(divide='ignore', invalid='ignore')
 
-'''
-When completed, this code will compute the posterior distribution for the memory constant.
-Here, we inject a waveform + memory model using GWMemory into LIGO modelled memory.
-'''
+np.seterr(divide="ignore", invalid="ignore")
+
+"""
+When completed, this code will compute the posterior distribution for the
+memory constant. Here, we inject a waveform + memory model using GWMemory
+into LIGO modelled memory.
+"""
 
 
 # Set the parameters of the data segment that we're
 # going to inject the signal into
-duration = 4.
-sampling_frequency = 2500.
-f_lower=15
+duration = 4.0
+sampling_frequency = 2500.0
+f_lower = 15
 
-# Specify the output directory and the name of the simulation !!!need to change!!!.
-outdir = '/home/darin/bilby_output'
-label = 'test1'
+# Specify the output directory and the name of the simulation.
+outdir = "/home/darin/bilby_output"
+label = "test1"
 bilby.core.utils.setup_logger(outdir=outdir, label=label)
 
 
-# Set up a random seed for result reproducibility.  This is optional!
+# Set up a random seed for result reproducibility. May or may not need this.
 np.random.seed(88170235)
 
-# Model with memory 
+# Model with memory
 def memory_model(M, S1, S2, d, inc, pol, memory_constant):
-  # Sample space definition for the memory's t-axis. Purposely set to begin, end, and have the same number of points as the
-  # original waveform so that superposition of the timeseries is possible.
-  start_time=-10
-  end_time=0.02
-  times = np.linspace(start_time, end_time, sampling_frequency*duration)
+    # Sample space definition for the memory's t-axis. Purposely set to begin,
+    # end, and have the same number of points as the original waveform so that
+    # superposition of the timeseries is possible.
+    start_time = -10
+    end_time = 0
+    times = np.linspace(start_time, end_time, sampling_frequency * duration)
 
+    # GW waveform with memory definition
+    # The sub-function waveforms.surrogate.Surrogate generates a surrogate
+    # object.
+    surr = gwmemory.waveforms.surrogate.Surrogate(
+        q=1,
+        name="nrsur7dq2",
+        spin_1=S1,
+        spin_2=S2,
+        total_mass=M,
+        distance=d,
+        times=times,
+    )
 
-  # GW waveform with memory definition
-  # The sub-function waveforms.surrogate.Surrogate generates a surrogate object.
-  surr = gwmemory.waveforms.surrogate.Surrogate(q=1, name='nrsur7dq2', spin_1=S1,
-                                                spin_2=S2, total_mass=M,
-                                                distance=d, times=times)
+    # GW waveform only definition
+    # A surrogate object has the following attributes: time_domain_memory (a
+    # pycbc.timeseries object that has both the ['plus'] and ['cross']
+    # sub-arrays), and time_domain_memory which also has ['plus'] and
+    # ['cross']). Calling these attributes returns both the pycbc timeseries
+    # and the sampling time arrays (which you store it as times here).
+    oscillatory, times = surr.time_domain_oscillatory(inc=inc, phase=pol)
 
+    # GW memory definition
+    memory, times = surr.time_domain_memory(inc=inc, phase=pol)
 
-  # GW waveform only definition
-  # A surrogate object has the following attributes: time_domain_memory (a 
-  # pycbc.timeseries object that has both the ['plus'] and ['cross'] sub-arrays), 
-  # and time_domain_memory which also has ['plus'] and ['cross']). Calling
-  # these attributes returns both the pycbc timesseries and the sampling time
-  # arrays (which you store it as times here).
-  oscillatory, times = surr.time_domain_oscillatory(inc=inc, phase=pol)
+    # waveform
+    plus = oscillatory["plus"][:] + memory_constant * memory["plus"][:]
+    cross = oscillatory["cross"][:] + memory_constant * memory["cross"][:]
 
-  # GW memory definition
-  memory, times = surr.time_domain_memory(inc=inc, phase=pol)
+    return {plus, cross}
 
-  # waveform
-  plus = oscillitory['plus'][:] + memory_constant*memory['plus'][:]
-  cross = oscillitory['cross'][:] + memory_constant*memory['cross'][:]
-
-  return {plus, cross}
 
 # We are going to inject a binary black hole waveform.  We first establish a
 # dictionary of parameters that includes all of the different waveform
 # parameters, including masses of the two black holes (mass_1, mass_2),
 # spins of both black holes (a, tilt, phi), etc.
-injection_parameters = dict(M=60., S1=[0., 0., 0.], S2=[0., 0., 0.],
-                            d=600., inc=np.pi/2, pol=0., geocent_time=0.,
-                            memory_constant=1.)
+injection_parameters = dict(
+    M=60.0,
+    S1=[0.0, 0.0, 0.0],
+    S2=[0.0, 0.0, 0.0],
+    d=600.0,
+    inc=np.pi / 2,
+    pol=0.0,
+    geocent_time=0.0,
+    memory_constant=1.0,
+)
 
 # Create the waveform_generator using a LAL BinaryBlackHole source function
 waveform_generator = bilby.gw.WaveformGenerator(
-    duration=duration, sampling_frequency=sampling_frequency,
-    time_domain_source_model=memory_model)
+    duration=duration,
+    sampling_frequency=sampling_frequency,
+    time_domain_source_model=memory_model,
+)
 
 # Set up interferometers.  In this case we'll use two interferometers
 # (LIGO-Hanford (H1), LIGO-Livingston (L1). These default to their design
 # sensitivity
-ifos = bilby.gw.detector.InterferometerList(['H1', 'L1', 'V1'])
+ifos = bilby.gw.detector.InterferometerList(["H1", "L1", "V1"])
 ifos.set_strain_data_from_power_spectral_densities(
-    sampling_frequency=sampling_frequency, duration=duration,
-    start_time=injection_parameters['geocent_time'] - 3)
-ifos.inject_signal(waveform_generator=waveform_generator,
-                   parameters=injection_parameters)
+    sampling_frequency=sampling_frequency,
+    duration=duration,
+    start_time=injection_parameters["geocent_time"] - 3,
+)
+ifos.inject_signal(
+    waveform_generator=waveform_generator, parameters=injection_parameters
+)
 
 # Set up a PriorDict, which inherits from dict.
 # By default we will sample all terms in the signal models.  However, this will
@@ -93,17 +113,24 @@ ifos.inject_signal(waveform_generator=waveform_generator,
 # distance, which means those are the parameters that will be included in the
 # sampler.  If we do nothing, then the default priors get used.
 priors = injection_parameters.copy()
-priors['memory_constant'] = bilby.core.prior.Uniform(0, 1, r'$\lambda$')
+priors["memory_constant"] = bilby.core.prior.Uniform(0, 1, r"$\lambda$")
 
 # Initialise the likelihood by passing in the interferometer data (ifos) and
 # the waveform generator
 likelihood = bilby.gw.GravitationalWaveTransient(
-    interferometers=ifos, waveform_generator=waveform_generator)
+    interferometers=ifos, waveform_generator=waveform_generator
+)
 
 # Run sampler.  In this case we're going to use the `dynesty` sampler
 result = bilby.run_sampler(
-    likelihood=likelihood, priors=priors, sampler='dynesty', npoints=500,
-    injection_parameters=injection_parameters, outdir=outdir, label=label)
+    likelihood=likelihood,
+    priors=priors,
+    sampler="dynesty",
+    npoints=500,
+    injection_parameters=injection_parameters,
+    outdir=outdir,
+    label=label,
+)
 
 # Make a corner plot.
 result.plot_corner()
--- working_scripts/memory_fd_v2.py
+++ working_scripts/memory_fd_v2.py
@@ -7,75 +7,93 @@ from matplotlib.pyplot import *
 import numpy as np
 import pandas as pd
 from matplotlib.pyplot import rcParams
-rcParams["font.family"]="Times New Roman"
-rcParams['axes.unicode_minus'] = False
-np.seterr(divide='ignore', invalid='ignore')
+
+rcParams["font.family"] = "Times New Roman"
+rcParams["axes.unicode_minus"] = False
+np.seterr(divide="ignore", invalid="ignore")
 
 
-'''
+"""
 This program calculates and plots asd's for waveforms and their
 associated memory. Comparison is then made with noise asd's by superposing plots.
 Noise asd's come from noise_curve.py
-'''
+"""
 
 
 # GWmemory natively features a frequency domain converter for memory but not for waveforms.
 # This method fixes that problem by following the same structure as frequency_domain_memory().
 def frequency_domain_transform(time_domain_strain, times):
-  sampling_frequency = 1 / (times[1] - times[0])
-  frequencies = None
-  frequency_domain_strain = dict()
+    sampling_frequency = 1 / (times[1] - times[0])
+    frequencies = None
+    frequency_domain_strain = dict()
 
-  for key in time_domain_strain:
-    frequency_domain_strain[key], frequencies =\
-      utils.nfft(time_domain_strain[key], sampling_frequency)
+    for key in time_domain_strain:
+        frequency_domain_strain[key], frequencies = utils.nfft(
+            time_domain_strain[key], sampling_frequency
+        )
 
-  return frequency_domain_strain, frequencies
+    return frequency_domain_strain, frequencies
 
 
 # Variable assignments for first plot
-f_lower=18.
-f_upper=313.
-S1 = [0., 0., 0.]
-S2 = [0., 0., 0.]
+f_lower = 18.0
+f_upper = 313.0
+S1 = [0.0, 0.0, 0.0]
+S2 = [0.0, 0.0, 0.0]
 inc = np.pi / 2
-pol = 0.
-d=420.
-M=66.2
-q=1.16
+pol = 0.0
+d = 420.0
+M = 66.2
+q = 1.16
 
 # Variable assignments for second plot
-f_lower_2=15.
-f_upper_2=265.
-S1_2 = [0., 0., 0.]
-S2_2 = [0., 0., 0.]
+f_lower_2 = 15.0
+f_upper_2 = 265.0
+S1_2 = [0.0, 0.0, 0.0]
+S2_2 = [0.0, 0.0, 0.0]
 inc_2 = np.pi / 2
-pol_2 = 0.
-d_2 = 20.
-M_2 = 80.
-q_2 = 1.
+pol_2 = 0.0
+d_2 = 20.0
+M_2 = 80.0
+q_2 = 1.0
 
 
 # Sample space definition for the memory's t-axis.
-start_time=-0.51
-end_time=0.02
+start_time = -0.51
+end_time = 0.02
 times = np.linspace(start_time, end_time, 10001)
 
 
 # GW waveform with memory definition
 # The sub-function waveforms.surrogate.Surrogate generates a surrogate object.
-surr = gwmemory.waveforms.surrogate.Surrogate(q=q, name='nrsur7dq2', spin_1=S1, spin_2=S2, total_mass=M, distance=d, times=times)
-surr_2 = gwmemory.waveforms.surrogate.Surrogate(q=q_2, name='nrsur7dq2', spin_1=S1_2, spin_2=S2_2, total_mass=M_2, distance=d_2, times=times)
+surr = gwmemory.waveforms.surrogate.Surrogate(
+    q=q,
+    name="nrsur7dq2",
+    spin_1=S1,
+    spin_2=S2,
+    total_mass=M,
+    distance=d,
+    times=times,
+)
+surr_2 = gwmemory.waveforms.surrogate.Surrogate(
+    q=q_2,
+    name="nrsur7dq2",
+    spin_1=S1_2,
+    spin_2=S2_2,
+    total_mass=M_2,
+    distance=d_2,
+    times=times,
+)
 
 
 # GW waveform-only definition
-# A surrogate object has the following attributes: frequency_domain_memory (a 
-# pycbc.frequencyseries object that has both the ['plus'] and ['cross'] sub-arrays), 
+# A surrogate object has the following attributes: frequency_domain_memory (a
+# pycbc.frequencyseries object that has both the ['plus'] and ['cross'] sub-arrays),
 # and frequency_domain_memory which also has ['plus'] and ['cross']). Calling
 # these attributes returns both the pycbc frequency series and the sampling
 # frequencies.
 
-# Now, basically you can still do the same (using time_domain_oscillatory) here. 
+# Now, basically you can still do the same (using time_domain_oscillatory) here.
 oscillatory, times = surr.time_domain_oscillatory(inc=inc, phase=pol)
 oscillatory_2, times_2 = surr_2.time_domain_oscillatory(inc=inc_2, phase=pol_2)
 
@@ -83,7 +101,9 @@ oscillatory_2, times_2 = surr_2.time_domain_oscillatory(inc=inc_2, phase=pol_2)
 # this to transform the osciallory time domain waveform to the frequency domain. We
 # use oscillatory_tilde, frequencies to store the products.
 oscillatory_tilde, frequencies = frequency_domain_transform(oscillatory, times)
-oscillatory_tilde_2, frequencies_2 = frequency_domain_transform(oscillatory_2, times_2)
+oscillatory_tilde_2, frequencies_2 = frequency_domain_transform(
+    oscillatory_2, times_2
+)
 
 # GW memory definition
 # Same thing happens for the memory
@@ -101,20 +121,24 @@ memory_tilde_2, frequencies_2 = frequency_domain_transform(memory_2, times_2)
 # In particular, you should plot frequencies as the x-axis, and the **magnitude**
 # of the amplitude_tilde times square root of frequencies as the y-axis.
 fig = figure(figsize=(6, 6))
-loglog(frequencies, abs(oscillatory_tilde['plus'][:]*np.sqrt(frequencies[:])), color='r')
+loglog(
+    frequencies,
+    abs(oscillatory_tilde["plus"][:] * np.sqrt(frequencies[:])),
+    color="r",
+)
 
 xlim(0, 5000)
-xlabel('Frequency [Hz]')
-ylabel(r'ASD [Hz$^{-1/2}$]')
-rc('xtick', labelsize=12)
-rc('ytick', labelsize=12)
-rc('axes', labelsize=14)
+xlabel("Frequency [Hz]")
+ylabel(r"ASD [Hz$^{-1/2}$]")
+rc("xtick", labelsize=12)
+rc("ytick", labelsize=12)
+rc("axes", labelsize=14)
 grid(False)
 
-savefig('waveform_asd_waveform.pdf')
+savefig("waveform_asd_waveform.pdf")
 
 tight_layout()
-#show()
+# show()
 close()
 
 
@@ -122,20 +146,24 @@ close()
 
 # Q5 : Do the same for the memory part.
 fig = figure(figsize=(6, 6))
-loglog(frequencies, abs(memory_tilde['plus'][:]*np.sqrt(frequencies[:])), color='r')
+loglog(
+    frequencies,
+    abs(memory_tilde["plus"][:] * np.sqrt(frequencies[:])),
+    color="r",
+)
 
 xlim(0, 5000)
-xlabel('Frequency [Hz]')
-ylabel(r'ASD [Hz$^{-1/2}$]')
-rc('xtick', labelsize=12)
-rc('ytick', labelsize=12)
-rc('axes', labelsize=14)
+xlabel("Frequency [Hz]")
+ylabel(r"ASD [Hz$^{-1/2}$]")
+rc("xtick", labelsize=12)
+rc("ytick", labelsize=12)
+rc("axes", labelsize=14)
 grid(False)
 
-savefig('waveform_asd_mem.pdf')
+savefig("waveform_asd_mem.pdf")
 
 tight_layout()
-#show()
+# show()
 close()
 
 
@@ -143,20 +171,27 @@ close()
 
 # Q6 : And of course, again for the waveform + memory.
 fig = figure(figsize=(6, 6))
-loglog(frequencies, abs((oscillatory_tilde['plus'][:] + memory_tilde['plus'][:])*np.sqrt(frequencies)), color='r')
+loglog(
+    frequencies,
+    abs(
+        (oscillatory_tilde["plus"][:] + memory_tilde["plus"][:])
+        * np.sqrt(frequencies)
+    ),
+    color="r",
+)
 
 xlim(0, 5000)
-xlabel('Frequency [Hz]')
-ylabel(r'ASD [Hz$^{-1/2}$]')
-rc('xtick', labelsize=12)
-rc('ytick', labelsize=12)
-rc('axes', labelsize=14)
+xlabel("Frequency [Hz]")
+ylabel(r"ASD [Hz$^{-1/2}$]")
+rc("xtick", labelsize=12)
+rc("ytick", labelsize=12)
+rc("axes", labelsize=14)
 grid(False)
 
-savefig('waveform_asd_wave_and_mem.pdf')
+savefig("waveform_asd_wave_and_mem.pdf")
 
 tight_layout()
-#show()
+# show()
 close()
 
 
@@ -166,82 +201,163 @@ close()
 f_new = np.zeros(len(frequencies))
 
 for i in range(len(frequencies)):
-  if frequencies[i] > f_upper:
-    f_new[i]=f_upper
-  elif frequencies[i] < f_lower:
-    f_new[i]=f_lower
-  else:
-    f_new[i]=frequencies[i]
+    if frequencies[i] > f_upper:
+        f_new[i] = f_upper
+    elif frequencies[i] < f_lower:
+        f_new[i] = f_lower
+    else:
+        f_new[i] = frequencies[i]
 
 f_new_2 = np.zeros(len(frequencies_2))
 
 for i in range(len(frequencies_2)):
-  if frequencies_2[i] > f_upper_2:
-    f_new_2[i]=f_upper_2
-  elif frequencies_2[i] < f_lower_2:
-    f_new_2[i]=f_lower_2
-  else:
-    f_new_2[i]=frequencies_2[i]
+    if frequencies_2[i] > f_upper_2:
+        f_new_2[i] = f_upper_2
+    elif frequencies_2[i] < f_lower_2:
+        f_new_2[i] = f_lower_2
+    else:
+        f_new_2[i] = frequencies_2[i]
 
 # Finally, do the same for the full plot.
-rc('xtick', labelsize=12)
-rc('ytick', labelsize=12)
-rc('axes', labelsize=14)
+rc("xtick", labelsize=12)
+rc("ytick", labelsize=12)
+rc("axes", labelsize=14)
 fig = figure(figsize=(9, 4))
 
 fig.add_subplot(1, 2, 1)
-loglog(f_new, abs((oscillatory_tilde['plus'][:] + memory_tilde['plus'][:])*np.sqrt(frequencies)), color='tab:orange', linewidth=8, zorder=1)
-#loglog(f_new, abs(oscillatory_tilde['plus']*np.sqrt(frequencies)), color='g', linewidth=12, zorder=1, label='Waveform')
-loglog(frequencies, abs(memory_tilde['plus']*np.sqrt(frequencies)), color='b', linewidth=8, zorder=1)
+loglog(
+    f_new,
+    abs(
+        (oscillatory_tilde["plus"][:] + memory_tilde["plus"][:])
+        * np.sqrt(frequencies)
+    ),
+    color="tab:orange",
+    linewidth=8,
+    zorder=1,
+)
+# loglog(f_new, abs(oscillatory_tilde['plus']*np.sqrt(frequencies)), color='g', linewidth=12, zorder=1, label='Waveform')
+loglog(
+    frequencies,
+    abs(memory_tilde["plus"] * np.sqrt(frequencies)),
+    color="b",
+    linewidth=8,
+    zorder=1,
+)
 
 # Finally, to plot the noise curve, simply load the txt files and plot them again here.
-dfl=pd.read_csv('L1_O2_Sensitivity_strain_asd.txt',sep="\t",index_col=False,header=None,names=["frequency","asd"])
-dfh=pd.read_csv('H1_O2_Sensitivity_strain_asd.txt',sep="\t",index_col=False,header=None,names=["frequency","asd"])
-dfv=pd.read_csv('V1_O2_Sensitivity_strain_asd.txt',sep="\s+",index_col=False,header=None, names=["frequency","asd"])
+dfl = pd.read_csv(
+    "L1_O2_Sensitivity_strain_asd.txt",
+    sep="\t",
+    index_col=False,
+    header=None,
+    names=["frequency", "asd"],
+)
+dfh = pd.read_csv(
+    "H1_O2_Sensitivity_strain_asd.txt",
+    sep="\t",
+    index_col=False,
+    header=None,
+    names=["frequency", "asd"],
+)
+dfv = pd.read_csv(
+    "V1_O2_Sensitivity_strain_asd.txt",
+    sep="\s+",
+    index_col=False,
+    header=None,
+    names=["frequency", "asd"],
+)
 
-loglog(dfl['frequency'], dfl['asd'], color='gwpy:ligo-livingston', zorder=0)
-loglog(dfh['frequency'], dfh['asd'], color='gwpy:ligo-hanford', zorder=0)
-loglog(dfv['frequency'], dfv['asd'], color='gwpy:virgo', zorder=0)
+loglog(dfl["frequency"], dfl["asd"], color="gwpy:ligo-livingston", zorder=0)
+loglog(dfh["frequency"], dfh["asd"], color="gwpy:ligo-hanford", zorder=0)
+loglog(dfv["frequency"], dfv["asd"], color="gwpy:virgo", zorder=0)
 
 xlim(20, 1000)
-ylim(5.5*(10**(-24)), 10**(-19))
-xlabel('Frequency [Hz]')
-ylabel(r'ASD [Hz$^{-1/2}$]')
-xscale('log')
-yscale('log')
-#legend(loc='upper right', prop={'size':11})
+ylim(5.5 * (10 ** (-24)), 10 ** (-19))
+xlabel("Frequency [Hz]")
+ylabel(r"ASD [Hz$^{-1/2}$]")
+xscale("log")
+yscale("log")
+# legend(loc='upper right', prop={'size':11})
 grid(False)
 
 
 # First plot was just GW150814. This plot ensures memory is detectable.
-rc('xtick', labelsize=12)
-rc('ytick', labelsize=12)
-rc('axes', labelsize=14)
+rc("xtick", labelsize=12)
+rc("ytick", labelsize=12)
+rc("axes", labelsize=14)
 fig.add_subplot(1, 2, 2)
-loglog(f_new_2, abs((oscillatory_tilde_2['plus'][:] + memory_tilde_2['plus'][:])*np.sqrt(frequencies_2)), color='tab:orange', linewidth=8, zorder=1, label='Waveform + Memory')
-#loglog(f_new, abs(oscillatory_tilde['plus']*np.sqrt(frequencies)), color='g', linewidth=12, zorder=1, label='Waveform')
-loglog(frequencies_2, abs(memory_tilde_2['plus']*np.sqrt(frequencies_2)), color='b', linewidth=8, zorder=1, label='Memory')
+loglog(
+    f_new_2,
+    abs(
+        (oscillatory_tilde_2["plus"][:] + memory_tilde_2["plus"][:])
+        * np.sqrt(frequencies_2)
+    ),
+    color="tab:orange",
+    linewidth=8,
+    zorder=1,
+    label="Waveform + Memory",
+)
+# loglog(f_new, abs(oscillatory_tilde['plus']*np.sqrt(frequencies)), color='g', linewidth=12, zorder=1, label='Waveform')
+loglog(
+    frequencies_2,
+    abs(memory_tilde_2["plus"] * np.sqrt(frequencies_2)),
+    color="b",
+    linewidth=8,
+    zorder=1,
+    label="Memory",
+)
 
-dfl=pd.read_csv('L1_O2_Sensitivity_strain_asd.txt',sep="\t",index_col=False,header=None,names=["frequency","asd"])
-dfh=pd.read_csv('H1_O2_Sensitivity_strain_asd.txt',sep="\t",index_col=False,header=None,names=["frequency","asd"])
-dfv=pd.read_csv('V1_O2_Sensitivity_strain_asd.txt',sep="\s+",index_col=False,header=None, names=["frequency","asd"])
+dfl = pd.read_csv(
+    "L1_O2_Sensitivity_strain_asd.txt",
+    sep="\t",
+    index_col=False,
+    header=None,
+    names=["frequency", "asd"],
+)
+dfh = pd.read_csv(
+    "H1_O2_Sensitivity_strain_asd.txt",
+    sep="\t",
+    index_col=False,
+    header=None,
+    names=["frequency", "asd"],
+)
+dfv = pd.read_csv(
+    "V1_O2_Sensitivity_strain_asd.txt",
+    sep="\s+",
+    index_col=False,
+    header=None,
+    names=["frequency", "asd"],
+)
 
-loglog(dfl['frequency'], dfl['asd'], color='gwpy:ligo-livingston', zorder=0 , label='Livingston')
-loglog(dfh['frequency'], dfh['asd'], color='gwpy:ligo-hanford', zorder=0 , label='Hanford')
-loglog(dfv['frequency'], dfv['asd'], color='gwpy:virgo', zorder=0, label='Virgo')
+loglog(
+    dfl["frequency"],
+    dfl["asd"],
+    color="gwpy:ligo-livingston",
+    zorder=0,
+    label="Livingston",
+)
+loglog(
+    dfh["frequency"],
+    dfh["asd"],
+    color="gwpy:ligo-hanford",
+    zorder=0,
+    label="Hanford",
+)
+loglog(
+    dfv["frequency"], dfv["asd"], color="gwpy:virgo", zorder=0, label="Virgo"
+)
 
 xlim(20, 1000)
-ylim(5.5*(10**(-24)), 10**(-19))
-xlabel('Frequency [Hz]')
-#ylabel(r'ASD [Hz$^{-1/2}$]')
-xscale('log')
-yscale('log')
-legend(loc='upper right', prop={'size':11})
+ylim(5.5 * (10 ** (-24)), 10 ** (-19))
+xlabel("Frequency [Hz]")
+# ylabel(r'ASD [Hz$^{-1/2}$]')
+xscale("log")
+yscale("log")
+legend(loc="upper right", prop={"size": 11})
 grid(False)
 
-savefig('waveform_asd_wave_and_mem_and_noise.pdf')
+savefig("waveform_asd_wave_and_mem_and_noise.pdf")
 
 tight_layout()
 show()
 close()
-
